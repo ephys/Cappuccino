bec5e83c01a0616b8cccec47577be1842d8c3171
ephys <ephyspotato@gmail.com>
2015-03-03 09:20:37 +0000

    Undo replacing the whole Dto package

---
+++ PAOO/src/paoo/cappuccino/business/dto/IUserDto.java
@@ -0,0 +1,13 @@
+package paoo.cappuccino.business.dto;
+
+/**
+ * Data transfer object for the User entity
+ *
+ * @author Nicolas Fischer
+ */
+public interface IUserDto extends IBaseDto {
+
+  public static enum Role {USER, ADMIN}
+
+  // TODO: reimplement, your eclipse fucked up and deleted the DTO folder. Was unable to fetch this class from svn :(
+}
