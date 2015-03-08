246c8327d5f31e31e85a7e1c9baf1602eb866c69
ephys <ephyspotato@gmail.com>
2015-03-08 18:17:10 +0000

    Added a few JUnits for the business layer

--- PAOO/test/paoo/cappuccino/business/entity/TestUserEntity.java
+++ PAOO/test/paoo/cappuccino/business/entity/TestUserEntity.java
@@ -10,6 +10,7 @@ import paoo.cappuccino.business.entity.factory.IEntityFactory;
 import paoo.cappuccino.core.AppContext;
 import paoo.cappuccino.core.injector.DependencyInjector;
 import paoo.cappuccino.core.injector.Inject;
+import paoo.cappuccino.util.hasher.IHashHolderDto;
 import paoo.cappuccino.util.hasher.IStringHasher;
 
 import static org.junit.Assert.assertEquals;
@@ -21,7 +22,6 @@ import static org.junit.Assert.assertTrue;
  * JUnit tests for the User entity.
  */
 public class TestUserEntity {
-  // TODO: do the same for the second factory method
 
   private static DependencyInjector injector;
 
@@ -31,7 +31,7 @@ public class TestUserEntity {
   private IStringHasher hasher;
 
   private String username = "Nicolas";
-  private char[] password = new char[] { 'p', 'o', 'm', 'm', 'e' };
+  private char[] password = "pomme".toCharArray();
   private String lastName = "Fischer";
   private String firstName = "Benoit";
   private String email = "nicolas@gmail.com";
@@ -77,9 +77,18 @@ public class TestUserEntity {
   }
 
   @Test
-  public void testPassword() {
+  public void testPasswordGetter() {
     assertTrue(hasher.matchHash(password, user.getPassword()));
-    assertFalse(hasher.matchHash(new char[] {'p', 'a', 'i', 'n'}, user.getPassword()));
+    assertFalse(hasher.matchHash("pain".toCharArray(), user.getPassword()));
+  }
+
+  @Test
+  public void testPasswordSetter() {
+    IHashHolderDto newPassword = hasher.hash("chocolatine".toCharArray());
+    user.setPassword(newPassword);
+
+    assertEquals("user.getPassword() did not return the password set by user.setPassword()",
+                 newPassword, user.getPassword());
   }
 
   @Test
@@ -90,5 +99,30 @@ public class TestUserEntity {
   @Test
   public void testUsername() {
     assertEquals(username, user.getUsername());
+  }
+
+  @Test
+  public void testVersionIncrementation() {
+    int version = user.getVersion();
+    int incrementedVersion = user.incrementVersion();
+
+    assertEquals(
+        "user.incrementVersion() (" + incrementedVersion + ") dit not match the older version ("
+        + version + ") + 1", version + 1, incrementedVersion);
+    assertEquals("user.getVersion() did not match the value returned by user.incrementVersion()",
+                 user.getVersion(), incrementedVersion);
+  }
+
+  @Test
+  public void testRoleEnum() {
+    // the hell ?
+
+    assertEquals("There somehow is more than 2 values in enum IUserDto.Role",
+                 IUserDto.Role.values().length, 2);
+
+    assertEquals("IUserDto.Role.valueOf did not return the right value (ADMIN)",
+                 IUserDto.Role.ADMIN, IUserDto.Role.valueOf("ADMIN"));
+    assertEquals("IUserDto.Role.valueOf did not return the right value (USER)",
+                 IUserDto.Role.USER, IUserDto.Role.valueOf("USER"));
   }
 }
