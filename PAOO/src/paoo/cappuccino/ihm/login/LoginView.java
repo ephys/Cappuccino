e6f8a42748b6f28f4026ebbc8a6496ed5e53d4af
Mathias Opsomer <opsomer.mathias@gmail.com>
2015-03-09 07:35:15 +0000

    ihm supression errorForm du login

--- PAOO/src/paoo/cappuccino/ihm/login/LoginView.java
+++ PAOO/src/paoo/cappuccino/ihm/login/LoginView.java
@@ -1,6 +1,5 @@
 package paoo.cappuccino.ihm.login;
 
-import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.JLabel;
@@ -12,7 +11,6 @@ import javax.swing.event.ChangeListener;
 
 import paoo.cappuccino.ihm.util.ErrorableTextField;
 import paoo.cappuccino.ihm.util.IhmConstants;
-import paoo.cappuccino.ihm.util.JLabelFont;
 
 /**
  * View for the Login Gui.
@@ -21,42 +19,43 @@ import paoo.cappuccino.ihm.util.JLabelFont;
  */
 public class LoginView extends JPanel implements ChangeListener {
 
-  private LoginModel model;
+	private LoginModel model;
 
-  private ErrorableTextField usernamePanel;
-  private ErrorableTextField passwordPanel;
-  private JLabel error;
+	private ErrorableTextField usernamePanel;
+	private ErrorableTextField passwordPanel;
+	private JLabel error;
 
-  /**
-   * Creates a new login view.
-   *
-   * @param model         The view's model.
-   * @param usernameField A text field for the username input.
-   * @param passwordField A password field for the password input.
-   */
-  public LoginView(LoginModel model, JTextField usernameField, JPasswordField passwordField) {
-    super(new GridLayout(3, 1, 0, IhmConstants.M_GAP));
+	/**
+	 * Creates a new login view.
+	 *
+	 * @param model
+	 *            The view's model.
+	 * @param usernameField
+	 *            A text field for the username input.
+	 * @param passwordField
+	 *            A password field for the password input.
+	 */
+	public LoginView(LoginModel model, JTextField usernameField,
+			JPasswordField passwordField) {
+		super(new GridLayout(2, 0, 0, IhmConstants.M_GAP));
 
-    this.model = model;
+		this.model = model;
 
-    this.usernamePanel = new ErrorableTextField(usernameField, "Nom d'utilisateur");
-    this.add(this.usernamePanel);
+		this.usernamePanel = new ErrorableTextField(usernameField,
+				"Nom d'utilisateur");
+		this.add(this.usernamePanel);
 
-    this.passwordPanel = new ErrorableTextField(passwordField, "Mot de passe");
-    this.add(this.passwordPanel);
+		this.passwordPanel = new ErrorableTextField(passwordField,
+				"Mot de passe");
+		this.add(this.passwordPanel);
 
-    this.error = new JLabelFont(null, 12);
-    this.error.setForeground(Color.RED);
-    this.add(error);
+		model.addChangeListener(this);
+		stateChanged(null);
+	}
 
-    model.addChangeListener(this);
-    stateChanged(null);
-  }
-
-  @Override
-  public void stateChanged(ChangeEvent event) {
-    usernamePanel.setError(model.getUsernameError());
-    passwordPanel.setError(model.getPasswordError());
-    error.setText(model.getFormError());
-  }
+	@Override
+	public void stateChanged(ChangeEvent event) {
+		usernamePanel.setError(model.getUsernameError());
+		passwordPanel.setError(model.getPasswordError());
+	}
 }
