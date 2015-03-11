94fb2db079007b53864523ce61b24338419d68c9
Laurent Batsle <laurent@batsle.be>
2015-03-11 12:25:18 +0000

    Créations des Ucc (not yet the methods)

---
+++ PAOO/src/paoo/cappuccino/ucc/IBusinessDayUcc.java
@@ -0,0 +1,14 @@
+package paoo.cappuccino.ucc;
+
+import paoo.cappuccino.core.injector.Singleton;
+
+/**
+ * Use case controller containing methods relative to a Business Day as an entity.
+ * 
+ * @author Laurent
+ *
+ */
+@Singleton
+public interface IBusinessDayUcc {
+
+}
---
+++ PAOO/src/paoo/cappuccino/ucc/ICompanyUcc.java
@@ -0,0 +1,14 @@
+package paoo.cappuccino.ucc;
+
+import paoo.cappuccino.core.injector.Singleton;
+
+/**
+ * Use case controller containing methods relative to a company as an entity.
+ * 
+ * @author Laurent
+ *
+ */
+@Singleton
+public interface ICompanyUcc {
+
+}
