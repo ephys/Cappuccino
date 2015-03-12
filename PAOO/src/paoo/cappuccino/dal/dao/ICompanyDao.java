0aca8a14aaeb27fe33f679956784ad4d75aedb06
Kevin Bavay <kbavay@yahoo.com>
2015-03-12 10:20:14 +0000

    businessDao, companyDao, contactDao : JavaDoc

--- PAOO/src/paoo/cappuccino/dal/dao/ICompanyDao.java
+++ PAOO/src/paoo/cappuccino/dal/dao/ICompanyDao.java
@@ -2,13 +2,39 @@ package paoo.cappuccino.dal.dao;
 
 public interface ICompanyDao {
 
-  //TODO JavaDoc
+  /**
+   * Inserts a new comany in the database.
+   *
+   * @param company The company to insert
+   * @return The company entity with its informations updated from the database
+   * @throws paoo.cappuccino.dal.exception.NonUniqueFieldException The name is not unique.
+   * @throws java.lang.IllegalArgumentException                    One of the fields failed to
+   *                                                               insert due to constraint
+   *                                                               violations.
+   * @throws paoo.cappuccino.dal.exception.ConnectionException     Database connection error
+   */
   //ICompanyDto createCompany(ICompanyDto company);
 
-  //TODO JavaDoc
-  //IParticipation[] fetchParticipationByDate(LocalDateTime time);
+  /**
+   * Fetche all the participations about the day in the database
+   *
+   * @param BusinessDay The day who we want the participation about (pas très anglais ma phrase)
+   * @return An that arrayList contains all the participation or null if none was found.
+   * @throws paoo.cappuccino.dal.exception.ConnectionException Database connection error
+   */
+  //ArrayList<IParticipation> fetchParticipationByDate(BusinessDay businessDay);
 
-  //TODO JavaDoc
-  //ICompanyDto fetchCompanyByName(String name, String postcode, String street, String town);
+  /**
+   * Fetche all the company matching all the not-null param.
+   *
+   * @param name     The name of the company
+   * @param postcode The postcode of the adress of the company
+   * @param street   The street of the adress of the company
+   * @param town     The town of the adress of the company
+   * @return An arrayList that contains all the company or null if none was found.
+   * @throws paoo.cappuccino.dal.exception.ConnectionException Database connection error
+   */
+  //ArrayList<ICompanyDto> fetchCompanyByName(String name, String postcode, String street,
+  //                                         String town);
 
 }
--- PAOO/src/paoo/cappuccino/dal/dao/IContactDao.java
+++ PAOO/src/paoo/cappuccino/dal/dao/IContactDao.java
@@ -2,13 +2,36 @@ package paoo.cappuccino.dal.dao;
 
 public interface IContactDao {
 
-  //TODO JavaDoc
+  /**
+   * Insert a new contact in the database
+   *
+   * @param contact The contact to insert
+   * @return the contact entity with its informations update from the database
+   * @throws java.lang.IllegalArgumentException                One of the fields failed to insert
+   *                                                           due to constraint violations.
+   * @throws paoo.cappuccino.dal.exception.ConnectionException Database connection error
+   */
   //IContactDto createContact(IContactDto contact);
 
-  //TODO JavaDoc
-  //IContactDto[] fetchContactsByCompany(int company);
+  /**
+   * Fetches all contacts whos comany_id matche with the @param company
+   *
+   * @param company The contact's company
+   * @return an arrayList with all contact find or a null arrayList if none was found
+   * @throws paoo.cappuccino.dal.exception.ConnectionException Database connection error
+   */
+  //ArrayList<IContactDto> fetchContactsByCompany(int company);
 
-  //TODO JavaDoc
-  //IContactDto[] fetchContactByName(String firstName, String lastName);
+  /**
+   * Fetches all contacts that matche the firstName and/or the lastName
+   *
+   * @param firstName The firstname of the contact
+   * @param lastName  The lastname of the contact
+   * @return An arrayList of contact whi matche the firstName and/or the lastName or a null
+   * arrayList if none was found
+   * @throws paoo.cappuccino.dal.exception.ConnectionException Database connection error
+   */
+  //ArrayList<IContactDto> fetchContactByName(String firstName, String lastName);
 
 }
+
