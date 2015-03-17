4c72ab28b16f5019c9d68c98cd230581e22003c8
Laurent Batsle <laurent@batsle.be>
2015-03-17 18:58:09 +0000

    CompanyUcc UP

---
+++ PAOO/src/paoo/cappuccino/ucc/impl/CompanyUcc.java
@@ -0,0 +1,41 @@
+package paoo.cappuccino.ucc.impl;
+
+import paoo.cappuccino.business.dto.ICompanyDto;
+import paoo.cappuccino.business.dto.IUserDto;
+import paoo.cappuccino.business.entity.factory.IEntityFactory;
+import paoo.cappuccino.dal.IDalService;
+import paoo.cappuccino.dal.dao.ICompanyDao;
+import paoo.cappuccino.ucc.ICompanyUcc;
+
+public class CompanyUcc implements ICompanyUcc {
+
+  private final IEntityFactory factory;
+  private final ICompanyDao dao;
+
+  public CompanyUcc(IEntityFactory entityFactory, IDalService dalService, ICompanyDao companyDao) {
+    this.factory = entityFactory;
+    this.dao = companyDao;
+  }
+
+  @Override
+  public ICompanyDto create(IUserDto creator, String name, String street, String numAdress,
+      String mailBox, String postCode, String town) {
+    ICompanyDto dto =
+        factory.createCompany(creator.getId(), name, street, numAdress, mailBox, postCode, town);
+    ICompanyDto returnedDto = dao.createCompany(dto);
+    return returnedDto;
+  }
+
+  @Override
+  public ICompanyDto[] searchCompanies(String name, String postCode, String town, String street) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ICompanyDto[] getInvitableCompanies() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
