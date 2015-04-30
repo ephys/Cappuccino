c4f5a1dea08f6b0e781df6881d2e8d69f101f143
Mathias Opsomer <opsomer.mathias@gmail.com>
2015-04-30 11:30:39 +0000

    debug

--- PAOO/lib/init.sql
+++ PAOO/lib/init.sql
@@ -1,4 +1,83 @@
 -- @author Kevin Bavay
+DROP SCHEMA IF EXISTS business_days CASCADE;
+
+CREATE SCHEMA business_days;
+
+CREATE SEQUENCE business_days.users_id_seq;
+CREATE TYPE business_days.user_role AS ENUM ('USER', 'ADMIN');
+CREATE TABLE IF NOT EXISTS business_days.users (
+  user_id       INTEGER PRIMARY KEY
+    DEFAULT NEXTVAL('business_days.users_id_seq'),
+  role          business_days.user_role                   NOT NULL DEFAULT 'USER',
+  password      VARCHAR(256)                NOT NULL,
+  email         VARCHAR(50)                 NOT NULL,
+  username      VARCHAR(25)                 NOT NULL UNIQUE,
+  first_name    VARCHAR(25)                 NOT NULL,
+  last_name     VARCHAR(25)                 NOT NULL,
+  register_date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+  version       INTEGER                     NOT NULL DEFAULT 1
+);
+
+CREATE SEQUENCE business_days.companies_id_seq;
+CREATE TABLE IF NOT EXISTS business_days.companies (
+  company_id       INTEGER PRIMARY KEY
+                                                                              DEFAULT NEXTVAL(
+                                                                                  'business_days.companies_id_seq'),
+  creator          INTEGER REFERENCES business_days.users (user_id)  NOT NULL,
+  name             VARCHAR(50)                                       NOT NULL UNIQUE,
+  register_date    TIMESTAMP WITHOUT TIME ZONE                       NOT NULL DEFAULT now(),
+  address_street   VARCHAR(50)                                       NOT NULL,
+  address_num      VARCHAR(10)                                       NOT NULL,
+  address_mailbox  VARCHAR(10),
+  address_postcode VARCHAR(10)                                       NOT NULL,
+  address_town     VARCHAR(25)                                       NOT NULL,
+  version          INTEGER                                           NOT NULL DEFAULT 1
+);
+
+CREATE SEQUENCE business_days.contacts_id_seq;
+CREATE TABLE IF NOT EXISTS business_days.contacts (
+  contact_id  INTEGER PRIMARY KEY
+                                                                                 DEFAULT NEXTVAL(
+                                                                                     'business_days.contacts_id_seq'),
+  company     INTEGER REFERENCES business_days.companies (company_id)   NOT NULL,
+  email       VARCHAR(50),
+  email_valid BOOLEAN                                                            DEFAULT TRUE,
+  first_name  VARCHAR(25)                                               NOT NULL,
+  last_name   VARCHAR(25)                                               NOT NULL,
+  phone       VARCHAR(25),
+  version     INTEGER                                                   NOT NULL DEFAULT 1
+);
+
+CREATE SEQUENCE business_days.business_days_id_seq;
+CREATE TABLE IF NOT EXISTS business_days.business_days (
+  business_day_id INTEGER PRIMARY KEY
+                                                       DEFAULT NEXTVAL(
+                                                           'business_days.business_days_id_seq'),
+  event_date      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  creation_date   TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+  academic_year   INTEGER                     NOT NULL UNIQUE,
+  version         INTEGER                     NOT NULL DEFAULT 1
+);
+
+CREATE TYPE business_days.participation_state AS ENUM ('INVITED', 'CONFIRMED', 'DECLINED', 'BILLED', 'PAID');
+CREATE TABLE IF NOT EXISTS business_days.participations (
+  company      INTEGER REFERENCES business_days.companies (company_id)           NOT NULL,
+  business_day INTEGER REFERENCES business_days.business_days (business_day_id)  NOT NULL,
+  state        business_days.participation_state                                               NOT NULL DEFAULT 'INVITED',
+  cancelled    BOOLEAN                                                           NOT NULL DEFAULT FALSE,
+  version      INTEGER                                                           NOT NULL DEFAULT 1,
+
+  PRIMARY KEY (company, business_day)
+);
+
+CREATE TABLE IF NOT EXISTS business_days.attendances (
+  company      INTEGER REFERENCES business_days.companies (company_id)          NOT NULL,
+  business_day INTEGER REFERENCES business_days.business_days (business_day_id) NOT NULL,
+  contact      INTEGER REFERENCES business_days.contacts (contact_id)           NOT NULL,
+
+  PRIMARY KEY (company, business_day, contact)
+);
+
 
 INSERT INTO business_days.users VALUES (DEFAULT, 'ADMIN', 'pbkdf2:7dbca107d1bc0416ae813ecaa44e443d0068b08c37ed4b4aa5d6d74c2d9dd33587a1d35504c8bbbc714920b4818b483d654efad4debb14ff34fd85464fe91d93:8214f59f6939c1e14dd30324790c4592:1000', 'dgrolaux@ipl.be', 'dgrolaux', 'Donatien', 'Grolaux', DEFAULT, DEFAULT);
 INSERT INTO business_days.users VALUES (DEFAULT, 'ADMIN', 'pbkdf2:7dbca107d1bc0416ae813ecaa44e443d0068b08c37ed4b4aa5d6d74c2d9dd33587a1d35504c8bbbc714920b4818b483d654efad4debb14ff34fd85464fe91d93:8214f59f6939c1e14dd30324790c4592:1000', 'blehmann@ipl.be', 'blehmann', 'Brigitte', 'Lehmann', DEFAULT, DEFAULT);
@@ -81,13 +160,16 @@ INSERT INTO business_days.participations VALUES (5, 3, 'DECLINED', DEFAULT, DEFA
 INSERT INTO business_days.participations VALUES (5, 4, 'DECLINED', DEFAULT, DEFAULT);
 INSERT INTO business_days.participations VALUES (5, 5, 'PAID', DEFAULT, DEFAULT);
 -- Contacts
-INSERT INTO business_days.contacts 7
+INSERT INTO business_days.contacts 
 VALUES (DEFAULT, 5, 'Virginie.BRASSINNE@bewan.be', DEFAULT, 'Virginie', 'Brassinne', NULL, DEFAULT);
-INSERT INTO business_days.contacts 8
+INSERT INTO business_days.contacts 
 VALUES (DEFAULT, 5, 'isabelle.croiset@bewan.be', DEFAULT, 'Isabelle', 'Croiset', NULL, DEFAULT);
-INSERT INTO business_days.contacts 9
+INSERT INTO business_days.contacts 
 VALUES (DEFAULT, 5, 'Dedecker drh@bewan.b', DEFAULT, 'Bénédicte', 'Dedecker', NULL, DEFAULT);
 -- Attendances
 INSERT INTO business_days.attendances VALUES (5, 2, 7);
 INSERT INTO business_days.attendances VALUES (5, 2, 8);
 INSERT INTO business_days.attendances VALUES (5, 5, 9);
+
+
+INSERT INTO business_days.users VALUES (DEFAULT, 'ADMIN', 'pbkdf2:81a2626cd1753863c79b61ddcec76a01200417296d3180652385111c051f67edbe25045b8ce1c6efc9596b0b7cec2cedc751110b9ee0f5521249872d0ed13714:49f148ee80fd772c7e63d2e784ff757c:1000', 'Ades@enfer.be', 'admin', 'Ades', 'Mars', DEFAULT, DEFAULT);
