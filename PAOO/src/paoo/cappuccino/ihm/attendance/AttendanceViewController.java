3cc60e81d348cc4215f162b6477d47e2e23b3d9a
ephys <ephyspotato@gmail.com>
2015-05-04 09:16:16 +0000

    same one

--- PAOO/src/paoo/cappuccino/ihm/attendance/AttendanceViewController.java
+++ PAOO/src/paoo/cappuccino/ihm/attendance/AttendanceViewController.java
@@ -213,7 +213,7 @@ public class AttendanceViewController extends JPanel implements ChangeListener,
             }
             if (contactsToAdd.size() == 0) {
               if (JOptionPane.showConfirmDialog(this, 
-            		  "Personne n'est selectionné. Valider ?") != JOptionPane.OK_OPTION) {
+                "Personne n'est selectionné. Valider ?") != JOptionPane.OK_OPTION) {
                 return;
               }
             }
